b6cdab7cccace91eed83a61e0ccdaa1a5fabbeeb
Hunter Wignall <hwignall2000@gmail.com>
2021-05-15 18:22:47 -0400

    Enable deploying to Nexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.0"
+    id("maven-publish")
+    id("java-library")
 }
 
 group = "me.hwiggy"
@@ -11,4 +13,35 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    repositories {
+        when (project.findProperty("deploy") ?: "local") {
+            "local" -> mavenLocal()
+            "remote" -> maven {
+                if (project.version.toString().endsWith("-SNAPSHOT")) {
+                    setUrl("https://nexus.mcdevs.us/repository/mcdevs-snapshots/")
+                    mavenContent { snapshotsOnly() }
+                } else {
+                    setUrl("https://nexus.mcdevs.us/repository/mcdevs-releases/")
+                    mavenContent { releasesOnly() }
+                }
+                credentials {
+                    username = System.getenv("NEXUS_USERNAME")
+                    password = System.getenv("NEXUS_PASSWORD")
+                }
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("assembly") {
+            from(components["java"])
+        }
+    }
 }
